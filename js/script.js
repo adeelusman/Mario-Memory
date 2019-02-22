0f2257084b4ab5d58d91a12bb2eb830d15d357d9
AdeelUsman <adeelusman10@gmail.com>
2019-02-23 04:19:22 +0500

    1.0
    Final game with all the css applied

--- js/script.js
+++ js/script.js
@@ -59,33 +59,33 @@ let firstGuess = '';
 let secondGuess = '';
 let previousTarget = null;
 let count = 0;
+let delay = 1200;
+
 // Grab the div with an id of root
 const game = document.getElementById('game');
-
-// Create a section with a class of grid
 const grid = document.createElement('section');
 grid.setAttribute('class', 'grid');
-
-// Append the grid section to the game div
 game.appendChild(grid);
 
-
 gameGrid.forEach(item => {
-  // Create a div and apply a card class
   const card = document.createElement('div');
   card.classList.add('card');
-
-  // Set the name and image attribute of the div to the cardsArray name and img
   card.dataset.name = item.name;
-  card.style.backgroundImage = `url(${item.img})`;
 
-  // Append the div to the grid section
+  const front = document.createElement('div');
+  front.classList.add('front');
+
+  const back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = `url(${item.img})`;
+
   grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
 });
 
-// Add match CSS
 const match = () => {
-  var selected = document.querySelectorAll('.selected');
+  let selected = document.querySelectorAll('.selected');
   selected.forEach(card => {
     card.classList.add('match');
   });
@@ -105,32 +105,28 @@ const resetGuesses = () => {
 // Add event listener to grid
 grid.addEventListener('click', function (event) {
   let clicked = event.target;
-  if (clicked.nodeName === 'SECTION' || clicked === previousTarget) {
-    return; }
+  if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('selected')) {
+    return;
+  }
   if (count < 2) {
     count++;
     if (count === 1) {
-      // Assign first guess
-      firstGuess = clicked.dataset.name;
-      clicked.classList.add('selected');
+      firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess);
+      clicked.parentNode.classList.add('selected');
     } else {
-      // Assign second guess
-      secondGuess = clicked.dataset.name;
-      clicked.classList.add('selected');
+      secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess);
+      clicked.parentNode.classList.add('selected');
     }
-    // If both guesses are not empty...
     if (firstGuess !== '' && secondGuess !== '') {
-      // and the first guess matches the second match...
       if (firstGuess === secondGuess) {
-        // run the match function
-        match();
-        resetGuesses();
+        setTimeout(match, delay);
+        setTimeout(resetGuesses, delay);
       } else {
-        resetGuesses();
-        }
+        setTimeout(resetGuesses, delay);
       }
-
-    // Set previous target to clicked
-  previousTarget = clicked;
-}
- });
+    }
+    previousTarget = clicked;
+  }
+});
